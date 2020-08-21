d2d697bbe397e30c12fa5cb430000db1827b0d2f
yljjgroup <tianzhou.lu@jiajiagroup.net>
2020-08-21 14:22:32 +0800

    add the support of markdown tables

--- conf.py
+++ conf.py
@@ -40,7 +40,7 @@ release = u'1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark','sphinxcontrib.mermaid']
+extensions = ['recommonmark','sphinxcontrib.mermaid','sphinx_markdown_tables']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
